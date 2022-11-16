a65b7ca655de08601c0161d645a2ef3e94894bdb
joebbrian <joebbrian@thezig.io>
2022-11-16 18:22:36 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useAcceptJs } from 'react-acceptjs';
+import React from 'react';
 
-function App() {
+const authData = {
+  apiLoginID: '',
+  clientKey: '',
+};
+const environment = "SANDBOX"
+// type BasicCardInfo = {
+//   cardNumber: string;
+//   cardCode: string;
+//   month: string;
+//   year: string;
+// };
+
+const App = () => {
+  const { dispatchData, loading, error } = useAcceptJs({authData });
+  const [cardData, setCardData] = React.useState({
+    cardNumber: '',
+    month: '',
+    year: '',
+    cardCode: '',
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // Dispatch CC data to Authorize.net and receive payment nonce for use on your server
+    const response = await dispatchData({ cardData });
+    console.log('Received response:', response);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="cardNumber"
+        placeholder="card number"
+        value={cardData.cardNumber}
+        onChange={(event) =>
+          setCardData({ ...cardData, cardNumber: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        name="month"
+        placeholder="month"
+        value={cardData.month}
+        onChange={(event) =>
+          setCardData({ ...cardData, month: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        name="year"
+        placeholder="year"
+        value={cardData.year}
+        onChange={(event) =>
+          setCardData({ ...cardData, year: event.target.value })
+        }
+      />
+      <input
+        type="text"
+        name="cardCode"
+        placeholder="code"
+        value={cardData.cardCode}
+        onChange={(event) =>
+          setCardData({ ...cardData, cardCode: event.target.value })
+        }
+      />
+      <button type="submit" disabled={loading || error}>
+        Pay
+      </button>
+    </form>
   );
-}
+};
 
 export default App;
